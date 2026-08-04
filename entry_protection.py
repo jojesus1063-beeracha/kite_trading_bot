de52dce199e54e00e52445fcbe89786547666656
jojesus1063-beeracha <jojesus1063@gmail.com>
2026-08-04 19:36:33 +0530

    Add market alignment diagnostics (entry_protection.py)

--- entry_protection.py
+++ entry_protection.py
@@ -24,6 +24,9 @@ SIGNAL_ANALYTICS_FIELDS = {
     "adx_delta",
     "relative_strength_score",
     "relative_strength_detail",
+    "market_trend_reason",
+    "sector_trend",
+    "sector_trend_reason",
 }
 
 
